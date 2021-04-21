28ce6cd357e1b0cc3d38f952765c8d2d7a9dba69
Marharyta <marharyta.dekret@gmail.com>
2021-04-21 10:16:40 +0300

    trying int datatype

--- cm_fit/training/cm_initialize.py
+++ cm_fit/training/cm_initialize.py
@@ -409,9 +409,10 @@ class CMInit(ulog.Loggable):
         y_pred = np.argmax(predictions, axis=3)
         classes = valid_generator.get_classes()
         y_true = np.argmax(classes, axis=3)
-        predictions = tf.cast(predictions, tf.float32)
+        predictions = tf.cast(predictions, tf.int16)
+        classes_f1 = tf.cast(classes, tf.int16)
 
-        f1_kmask = round(self.model.custom_f1(classes, predictions), 2)
+        f1_kmask = round(self.model.custom_f1(classes_f1, predictions), 2)
 
         y_pred_fl = y_pred.flatten()
         y_true_fl = y_true.flatten()
@@ -426,8 +427,9 @@ class CMInit(ulog.Loggable):
 
         sen2cor = valid_generator.get_sen2cor()
         y_sen2cor = np.argmax(sen2cor, axis=3)
-        sen2cor = tf.cast(sen2cor, tf.float32)
-        f1_sen2cor = round(self.model.custom_f1(classes, sen2cor), 2)
+        sen2cor = tf.cast(sen2cor, tf.int16)
+        classes_f1 = tf.cast(classes, tf.int16)
+        f1_sen2cor = round(self.model.custom_f1(classes_f1, sen2cor), 2)
         y_sen2cor_fl = y_sen2cor.flatten()
         y_true_fl = y_true.flatten()
         unique_true = np.unique(y_true_fl)
