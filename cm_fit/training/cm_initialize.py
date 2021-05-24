4d874047321e22ca69c2d31f62f90135746a6955
Marharyta <marharyta.dekret@gmail.com>
2021-05-24 17:37:19 +0300

    default parameters fill

--- cm_fit/training/cm_initialize.py
+++ cm_fit/training/cm_initialize.py
@@ -387,7 +387,8 @@ class CMFit(ulog.Loggable):
         self.model.set_learning_rate(self.learning_rate)
 
         # Construct and compile the model.
-        self.model.construct(self.dim[0], self.dim[1], len(self.features), len(self.classes), pretrained_weights)
+        self.model.construct(self.dim[0], self.dim[1], len(self.features), len(self.classes), layers=5, units=64,
+                             pretrained_weights=pretrained_weights)
         self.model.model.summary()
         self.model.compile(self.loss_name)
 
