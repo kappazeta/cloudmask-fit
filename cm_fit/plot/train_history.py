ba67e4cf518838d3f002296e7a5a4216806766f1
Marharyta <marharyta.dekret@gmail.com>
2021-05-27 19:18:13 +0300

    plot smaller fix

--- cm_fit/plot/train_history.py
+++ cm_fit/plot/train_history.py
@@ -58,7 +58,7 @@ def plot_confusion_matrix(cm, class_list, title, normalized=False, cmap=plt.cm.B
     Based on https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
     """
     if smaller:
-        cm_smaller = cm[1:5, 1:5]
+        cm_smaller = cm[0:5, 0:5]
     else:
         cm_smaller = cm
     print(cm_smaller)
